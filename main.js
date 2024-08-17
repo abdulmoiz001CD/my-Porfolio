6b1e9a40543133d6ee0d412fb3e663f30db93da7
AbdulMoiz <abdulmoizs2005@gmail.com>
2024-08-17 06:17:54 -0700

    first

---
+++ main.js
@@ -0,0 +1,23 @@
+const ulist_firstEL = document.getElementById("ulist_first");
+const barEL = document.getElementById("bar");
+const iconbarEL = document.getElementById("iconbar");
+
+const barControler = () => {
+
+    if(ulist_firstEL.style.display=== "block"){
+        ulist_firstEL.style.display= "none"
+         barEL.style.backgroundColor="#0089b7"
+        iconbarEL.style.fontSize ="16px"
+    }else{
+        ulist_firstEL.style.display="block"
+        barEL.style.backgroundColor="#003251"
+        iconbarEL.style.fontSize ="10px"
+    }
+
+    
+
+}
+
+iconbarEL.addEventListener("click",barControler)
+
+
