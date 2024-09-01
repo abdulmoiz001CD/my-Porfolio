5f21e3320b5400b9a07bb33df2ed63c7db018333
AbdulMoiz <abdulmoizs2005@gmail.com>
2024-09-01 05:06:26 -0700

    Update barIcon

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ const barControler = () => {
     if(ulist_firstEL.style.display=== "block"){
         ulist_firstEL.style.display= "none"
          barEL.style.backgroundColor="#0089b7"
-        iconbarEL.style.fontSize ="16px"
+        iconbarEL.style.fontSize ="17px"
     }else{
         ulist_firstEL.style.display="block"
         barEL.style.backgroundColor="#003251"
-        iconbarEL.style.fontSize ="10px"
+        iconbarEL.style.fontSize ="17px"
     }
 
     
